1fd83e14fbc13384a9073489c39d824ead249395
DinaAkilaDev <dinashadiakeela@gmail.com>
2021-10-03 09:04:03 +0300

    movie

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -1,50 +1,50 @@
-<script src="{{ URL::to('../assets/global/plugins/respond.min.js') }}"></script>
-<script src="{{ URL::to('../assets/global/plugins/excanvas.min.js') }}"></script>
-<script src="{{ URL::to('../assets/global/plugins/ie8.fix.min.js') }}"></script>
-<script src="{{ URL::to('../assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/js.cookie.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jquery-validation/js/additional-methods.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/select2/js/select2.full.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/pages/scripts/login.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/moment.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/morris/morris.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/morris/raphael-min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/counterup/jquery.waypoints.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/counterup/jquery.counterup.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/fullcalendar/fullcalendar.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/horizontal-timeline/horizontal-timeline.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/flot/jquery.flot.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/flot/jquery.flot.resize.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/flot/jquery.flot.categories.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jquery.sparkline.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js"') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/pages/scripts/dashboard.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/layouts/layout5/scripts/layout.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/layouts/global/scripts/quick-nav.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js') }}"></script>
-<script src="{{ URL::to('../assets/global/scripts/datatable.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/datatables/datatables.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/global/scripts/app.min.') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/pages/scripts/table-datatables-editable.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/layouts/layout5/scripts/layout.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::to('../assets/layouts/global/scripts/quick-nav.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/respond.min.js') }}"></script>
+<script src="{{ asset('../assets/global/plugins/excanvas.min.js') }}"></script>
+<script src="{{ asset('../assets/global/plugins/ie8.fix.min.js') }}"></script>
+<script src="{{ asset('../assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/js.cookie.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jquery-validation/js/additional-methods.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/select2/js/select2.full.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/pages/scripts/login.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/moment.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/morris/morris.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/morris/raphael-min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/counterup/jquery.waypoints.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/counterup/jquery.counterup.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/fullcalendar/fullcalendar.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/horizontal-timeline/horizontal-timeline.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/flot/jquery.flot.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/flot/jquery.flot.resize.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/flot/jquery.flot.categories.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jquery.sparkline.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js"') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/pages/scripts/dashboard.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/layouts/layout5/scripts/layout.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/layouts/global/scripts/quick-nav.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js') }}"></script>
+<script src="{{ asset('../assets/global/scripts/datatable.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/datatables/datatables.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/global/scripts/app.min.') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/pages/scripts/table-datatables-editable.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/layouts/layout5/scripts/layout.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('../assets/layouts/global/scripts/quick-nav.min.js') }}" type="text/javascript"></script>
 
 
